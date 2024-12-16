427c2fbaafce6757c001f7b653078d17054b9612
Sarankon Hiranphonkrit (Home) <sarankon.hi@gmail.com>
2024-12-17 01:03:26 +0700

    add server and enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,13 @@ async function bootstrap() {
     const app = await NestFactory.create(AppModule)
     // const app = await NestFactory.create<NestFastifyApplication>(AppModule);
 
+    app.enableCors()
+
     const config = new DocumentBuilder()
         .setTitle("Seed Nest 10 (Example)")
         .setDescription("Seed Nest API Description")
         .setVersion("1.0")
+        .addServer("http://localhost:3000")
         .addBasicAuth()
         .addBearerAuth()
         .build()
