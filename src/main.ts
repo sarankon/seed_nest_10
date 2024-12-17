f797a3d99e3c6c069f3c27fae464bfeadc5a9a2a
Sarankon Hiranphonkrit (Home) <sarankon.hi@gmail.com>
2024-12-18 02:10:24 +0700

    provide static file

--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,16 @@ import { NestFactory } from "@nestjs/core"
 import { AppModule } from "./app/app.module"
 // import { NestFastifyApplication } from '@nestjs/platform-fastify';
 import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
+import { NestExpressApplication } from "@nestjs/platform-express"
+import { join } from "path"
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule)
+    // const app = await NestFactory.create(AppModule)
+    const app = await NestFactory.create<NestExpressApplication>(AppModule)
     // const app = await NestFactory.create<NestFastifyApplication>(AppModule);
 
     app.enableCors()
+    app.useStaticAssets(join(__dirname, '..', 'public'))
 
     const config = new DocumentBuilder()
         .setTitle("Seed Nest 10 (Example)")
