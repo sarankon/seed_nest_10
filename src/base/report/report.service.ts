ab093b600b0a70b7e5bf0ce88f2edc575d3e3fd4
Sarankon Hiranphonkrit (Home) <sarankon.hi@gmail.com>
2025-01-27 00:12:59 +0700

    update report module

--- src/base/report/report.service.ts
+++ src/base/report/report.service.ts
@@ -1,13 +1,18 @@
-import { Injectable } from "@nestjs/common"
+import { BadRequestException, Injectable } from "@nestjs/common"
 import { ConfigService } from "@nestjs/config"
 import { EntityManager } from "@mikro-orm/core"
 import { InjectEntityManager } from "@mikro-orm/nestjs"
 
+import { v4 as uuidv4 } from "uuid"
+import { ResponseBody } from "../response-body"
+
 import { writeFileSync } from "fs"
 import * as ExcelJS from "exceljs"
 import { PDFDocument, StandardFonts, rgb } from "pdf-lib"
+import { _ReportPDF } from "./entities/report-pdf.entity"
+import { _ReportXLSX } from "./entities/report-xlsx.entity"
 
-import reportConfig from "../../config/report.config"
+// import reportConfig from "../../config/report.config"
 
 @Injectable()
 export class ReportService {
@@ -21,46 +26,98 @@ export class ReportService {
     async exportCSV() {}
 
     async exportXLSX() {
-        const workbook = new ExcelJS.Workbook()
-        // workbook.creator = "Me"
-        // workbook.lastModifiedBy = "Her"
-        // workbook.created = new Date(1985, 8, 30)
-        // workbook.modified = new Date()
-        // workbook.lastPrinted = new Date(2016, 9, 27)
+        try {
+            const workbook = new ExcelJS.Workbook()
+            workbook.creator = "System"
+            workbook.lastModifiedBy = "System"
+            workbook.created = new Date()
+            workbook.modified = new Date()
 
-        const sheet1 = workbook.addWorksheet("Sheet 1")
-        const sheet2 = workbook.addWorksheet("Sheet 2")
-        const sheet3 = workbook.addWorksheet("Sheet 1")
+            const sheet1 = workbook.addWorksheet("Sheet 1")
 
-        workbook.xlsx.writeFile("./file.xlsx")
+            // ----- File Name And Path
+            const fileName = uuidv4() + ".xlsx"
+            const xlsxSavePath = this.configService.get<string>("XLSX_PATH")
+            const xlsxFileUrl = this.configService.get<string>("XLSX_URL")
+
+            // ----- Write File
+            workbook.xlsx.writeFile(xlsxSavePath + fileName)
+
+            // ----- Save Data To Database
+            const reportXLSX = new _ReportXLSX()
+            reportXLSX.name = fileName
+            reportXLSX.originalFileName = fileName
+            reportXLSX.filePath = xlsxSavePath
+            reportXLSX.fileUrl = xlsxFileUrl + fileName
+            reportXLSX.mimeTypes = "application/vnd.ms-excel"
+            await this.em.persist(reportXLSX).flush()
+
+            const hostUrl = this.configService.get<string>("HOST_URL")
+            reportXLSX.fileUrl = hostUrl + reportXLSX.fileUrl
+
+            return new ResponseBody(200, reportXLSX)
+        } catch (err) {
+            console.error("Error:", err)
+            throw new BadRequestException({
+                statusCode: 400,
+                error: err.sqlMessage,
+                message: "",
+            })
+        }
     }
 
     async exportPDF() {
-        // Create a new PDFDocument
-        const pdfDoc = await PDFDocument.create()
+        try {
+            // Create a new PDFDocument
+            const pdfDocument = await PDFDocument.create()
+            // Add a blank page to the document
+            const page = pdfDocument.addPage()
+            // Get the width and height of the page
+            const { width, height } = page.getSize()
 
-        // Embed the Times Roman font
-        const timesRomanFont = await pdfDoc.embedFont(StandardFonts.TimesRoman)
+            // Embed the Times Roman font
+            const timesRomanFont = await pdfDocument.embedFont(StandardFonts.TimesRoman)
+            // Draw a string of text toward the top of the page
+            const fontSize = 30
+            page.drawText("Creating PDFs in JavaScript is awesome!", {
+                x: 50,
+                y: height - 4 * fontSize,
+                size: fontSize,
+                font: timesRomanFont,
+                color: rgb(0, 0.53, 0.71),
+            })
 
-        // Add a blank page to the document
-        const page = pdfDoc.addPage()
+            // Serialize the PDFDocument to bytes (a Uint8Array)
+            const pdfBytes = await pdfDocument.save()
 
-        // Get the width and height of the page
-        const { width, height } = page.getSize()
+            // ----- File Name And Path
+            const fileName = uuidv4() + ".pdf"
+            const pdfSavePath = this.configService.get<string>("PDF_PATH")
+            const pdfFileUrl = this.configService.get<string>("PDF_URL")
 
-        // Draw a string of text toward the top of the page
-        const fontSize = 30
-        page.drawText("Creating PDFs in JavaScript is awesome!", {
-            x: 50,
-            y: height - 4 * fontSize,
-            size: fontSize,
-            font: timesRomanFont,
-            color: rgb(0, 0.53, 0.71),
-        })
+            // ----- Write File
+            writeFileSync(pdfSavePath + fileName, pdfBytes)
 
-        // Serialize the PDFDocument to bytes (a Uint8Array)
-        const pdfBytes = await pdfDoc.save()
+            // ----- Save Data To Database
+            const reportPDF = new _ReportPDF()
+            reportPDF.name = fileName
+            reportPDF.originalFileName = fileName
+            reportPDF.filePath = pdfSavePath
+            reportPDF.fileUrl = pdfFileUrl + fileName
+            reportPDF.mimeTypes = "application/pdf"
+            await this.em.persist(reportPDF).flush()
 
-        await writeFileSync("./file.pdf", pdfBytes)
+            const hostUrl = this.configService.get<string>("HOST_URL")
+            reportPDF.fileUrl = hostUrl + reportPDF.fileUrl
+
+            return new ResponseBody(200, reportPDF)
+        } catch (err) {
+            console.error("Error:", err)
+            throw new BadRequestException({
+                statusCode: 400,
+                error: err.sqlMessage,
+                message: "",
+            })
+        }
     }
 }
