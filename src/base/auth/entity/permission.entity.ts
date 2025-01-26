80a2069a2481ac750a517bca55723031320be891
Sarankon Hiranphonkrit (Home) <sarankon.hi@gmail.com>
2025-01-27 00:12:24 +0700

    update permission entity

---
+++ src/base/auth/entity/permission.entity.ts
@@ -0,0 +1,14 @@
+import { Entity, Property } from "@mikro-orm/core"
+import { BaseEntity } from "src/base/base.entity"
+
+@Entity()
+export class _Permission extends BaseEntity {
+    @Property({ columnType: "uuid", unique: "uuid" })
+    uuid!: string
+
+    @Property({ columnType: "character varying(250)", unique: "name" })
+    name!: string
+
+    @Property({ columnType: "character varying(250)" })
+    description?: string
+}
